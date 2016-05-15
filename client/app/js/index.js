9dfc9a1af795f6bc295e4094be8503d2e62a1a41
Juan Facorro <juan.facorro@klarna.com>
2016-05-15 08:48:48 +0200

    Change configuration format. Allow code to be executed after dom-ready.

--- client/app/js/index.js
+++ client/app/js/index.js
@@ -7,13 +7,14 @@ $(function () {
     let screens = remote.getGlobal('screens');
     let id = getParameterByName('screen');
     let screen = screens[id];
+    var webviews = null;
 
     switch(screen.layout.name) {
     case 'carrousel':
-        carrousel(screen);
+        webviews = carrousel(screen);
         break;
     case 'splitted':
-        splitted(screen);
+        webviews = splitted(screen);
         break;
     }
 });
@@ -26,21 +27,23 @@ function getParameterByName(name) {
 function carrousel(screen) {
     let webviews = [];
 
-    for(var i = 0; i < screen.urls.length; i++) {
+    for(var i = 0; i < screen.pages.length; i++) {
         let webview = $('<webview/>')
-                .attr('src', screen.urls[i])
+                .attr('src', screen.pages[i].url)
                 .attr('style', 'width:100%; height: 100%');
         let container = $('<div></div>')
                 .addClass('container')
                 .addClass(i === 0 ? 'top' : 'bottom')
-                .append(webview)
-                .prop('webview', webview.get(0));
+                .append(webview);
 
         $('#view').append(container);
+
+        executeOnLoad(webview, screen.pages[i]);
 
         webviews.push(container);
     }
     rotate(webviews, screen.layout.time);
+    return webviews;
 }
 
 function rotate(webviews, every) {
@@ -56,11 +59,13 @@ function rotate(webviews, every) {
 
 function splitted(screen) {
     let orientation = screen.layout.orientation;
-    let count = screen.urls.length;
+    let count = screen.pages.length;
     let percentage = 100 / count;
+    let webviews = [];
 
     for(var i = 0; i < count; i++) {
-        let webview = $('<webview/>').attr('src', screen.urls[i]);
+        console.log(screen.pages[i]);
+        let webview = $('<webview/>').attr('src', screen.pages[i].url);
         $('#view').append(webview);
 
         let width  = (orientation == 'horizontal' ? 100 : percentage);
@@ -69,5 +74,22 @@ function splitted(screen) {
                 + ' width: ' + width + '%;'
                 + ' height:' + height + '%';
         webview.attr('style', style);
+
+        executeOnLoad(webview, screen.pages[i]);
+
+        webviews.push(webview);
+    }
+    return webviews;
+
+}
+
+function executeOnLoad(webview, page) {
+    if(page.code !== undefined) {
+        console.log(page.code);
+        let code = page.code;
+        let webviewObj = webview.get(0);
+        webview.on('dom-ready', function() {
+            webviewObj.executeJavaScript(code);
+        });
     }
 }
